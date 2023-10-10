6ad5d42fa9b9965f371a3944fdab6d95af4069fe
DenisC <denis0ches00@gmail.com>
2023-10-10 16:20:31 +0700

    UTF8 base64

--- je/advanced/crypt.js
+++ je/advanced/crypt.js
@@ -1,19 +1,9 @@
 export const Crypt = {
     decodeBase64: (data) => {
-        try {
-            return atob(data);
-        }
-        catch (_a) {
-            return data;
-        }
+        return new TextDecoder().decode(atob(data));
     },
     encodeBase64: (data) => {
-        try {
-            return btoa(data);
-        }
-        catch (_a) {
-            return data;
-        }
+        return btoa(new TextEncoder().encode(data));
     },
     reverse: (data) => data.split('').reverse().join('')
 };
