8abe396d6eab6f8e40f8845e9a22fa0412ff8a86
DenisC <denis0ches00@gmail.com>
2023-10-11 18:30:29 +0700

    d

--- src/index.js
+++ src/index.js
@@ -2,25 +2,7 @@ import { Canvas } from '../je/index.js';
 
 const cv = new Canvas({
     width: 1366,
-    height: 768,
-    fullscreen: true
+    height: 768
 });
-
-import { Instance } from '../je/index.js';
-
-class Test extends Instance {
-    onUpdate() {
-        test.pos = cv.center.copy();
-    }
-
-    onDraw() {
-        this.fillColor = 'red';
-        this.circle(0, 0, 100);
-        this.fill();
-    }
-}
-
-const test = new Test();
-test.addToMain();
 
 cv.start();
