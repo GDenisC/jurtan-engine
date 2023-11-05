d7e9900a37881b2e392c7d111ddb32e568f283cf
DenisC <denis0ches00@gmail.com>
2023-11-05 12:37:15 +0700

    clear

--- je/instance.js
+++
@@ -1,2 +0,0 @@
-"use strict";
-//# sourceMappingURL=instance.js.map
--- je/scene.d.ts
+++
@@ -1,8 +0,0 @@
-import { Instance } from "./instance.js";
-/**
- * A scene is a collection of instances
- * Scene always is a child of Canvas
- */
-export declare class Scene extends Instance {
-    constructor();
-}
